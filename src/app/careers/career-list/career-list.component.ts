4c0fbe5519bbc70efbc00dfb220b11d6f61bfdd4
Apoh Eldrige <peldrige8@gmail.com>
2020-10-19 00:36:48 +0100

    added seearching functionality to career-list component

--- src/app/careers/career-list/career-list.component.ts
+++ src/app/careers/career-list/career-list.component.ts
@@ -8,11 +8,24 @@ import { ICareer } from '../../shared/career';
   styleUrls: ['./career-list.component.css'],
 })
 export class CareerListComponent implements OnInit {
+  constructor(private careerServ: CareerService) {}
   // map careerArray to take the career interface
   careerArray: ICareer[] = [];
+  filteredCareerArray: ICareer[];
 
-  constructor(private careerServ: CareerService) {}
+  // falsy initially
+  _listFilter: string = '';
 
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredCareerArray = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.careerArray;
+  }
   ngOnInit(): void {
     this.getCareers();
     //call getCareers() on cretion of component
@@ -22,10 +35,19 @@ export class CareerListComponent implements OnInit {
   getCareers() {
     this.careerServ.getCareers().subscribe(
       (data) => {
-        (this.careerArray = data), console.log(this.careerArray);
+        (this.careerArray = data),
+          (this.filteredCareerArray = this.performFilter(this.listFilter));
       },
 
       (err) => console.error(err)
+    );
+  }
+
+  // fiter careerArray based on input
+  performFilter(filterBy: string): ICareer[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.careerArray.filter(
+      (career) => career.title.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
   }
 }
