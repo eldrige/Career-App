e67bb5c3accefe98461641ddbe40b006eeec5523
Apoh Eldrige <peldrige8@gmail.com>
2021-01-12 02:51:39 +0100

    basic api setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 const app = express();
-const PORT = process.env.PORT || 4000;
-import careerRouter from "./routes/careerRoutes.js";
+import careerRouter from './routes/careerRoutes.js';
+dotenv.config();
+app.use(morgan('dev'));
 
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.send("API is running");
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
 
-app.use("/api/careers/", careerRouter);
+app.use('/api/careers/', careerRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
