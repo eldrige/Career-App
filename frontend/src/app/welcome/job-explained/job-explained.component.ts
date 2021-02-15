49f0f102a883b40a3e1bbfb487a3aabf86d8e6e2
yunweneric <yunweneric@gmail.com>
2021-02-15 18:14:00 +0100

    modified UI of landing page

---
+++ frontend/src/app/welcome/job-explained/job-explained.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-explained',
+  templateUrl: './job-explained.component.html',
+  styleUrls: ['./job-explained.component.css']
+})
+export class JobExplainedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
