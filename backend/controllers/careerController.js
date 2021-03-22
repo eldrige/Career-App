687bf8c1b9b2bc5c5a37e303bb9ee7361a2ffeb0
Apoh Eldrige <peldrige8@gmail.com>
2021-03-22 15:16:01 +0100

    create career controller

---
+++ backend/controllers/careerController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import Career from '../models/careerModel.js';
+
+// @desc fetch all careers
+// @route GET /api/careers
+// @access public
+const getCareers = asyncHandler(async (req, res) => {
+  const careers = await Career.find({});
+  res.json(careers);
+});
+
+// @desc fetch one careers
+// @route GET /api/careers/:id
+// @access public
+const getCareerById = asyncHandler(async (req, res) => {
+  const career = await Career.findById(req.params.id);
+
+  if (career) {
+    res.json(career);
+  } else {
+    res.status(404).json({ message: 'Career not found' });
+  }
+});
+
+export { getCareers, getCareerById };
+
+// ! controllers just encapsulate the logic
--- backend/routes/careerRoutes.js
+++ backend/routes/careerRoutes.js
@@ -1,34 +1,10 @@
 import express from 'express';
-import Career from '../models/careerModel.js';
-import asyncHandler from 'express-async-handler';
-const careerRouter = express.Router();
-// import careers from '../data/careers.js';
 
-// @desc fetch all careers
-// @route GET /api/careers
-// @access public
-careerRouter.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const careers = await Career.find({});
-    res.json(careers);
-  })
-);
-// @desc fetch one careers
-// @route GET /api/careers/:id
-// @access public
+const router = express.Router();
+import { getCareers, getCareerById } from '../controllers/careerController.js';
 
-careerRouter.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const career = await Career.findById(req.params.id);
+router.route('/').get(getCareers);
+//  ? this is equivalent to router.get('/', getProducts)
+router.route('/:id').get(getCareerById);
 
-    if (career) {
-      res.json(career);
-    } else {
-      res.status(404).json({ message: 'Career not found' });
-    }
-  })
-);
-
-export default careerRouter;
+export default router;
