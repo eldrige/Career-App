374ea5a21d951438784a32cf5ab472c14f87eccf
eldrige <peldrige8@gmail.com>
2021-04-16 03:37:11 +0100

    show loggedin user in navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -4,14 +4,14 @@ import { Renderer2, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  user;
   ngOnInit(): void {
+    this.user = localStorage.getItem('currentUser')
+      ? JSON.parse(localStorage.getItem('currentUser'))
+      : false;
   }
-
 }
-
