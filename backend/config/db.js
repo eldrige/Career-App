dce855579334db5db55749cdfad41431c64ce91b
eldrige <peldrige8@gmail.com>
2021-04-21 14:24:56 +0100

    changed app name to Jongleur

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb://127.0.0.1/Jongleur', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, 'abc123', {
     expiresIn: '30d',
   });
 };
