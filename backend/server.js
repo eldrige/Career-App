e58a270d16313220e2d1a7dc898f156d46c2cfc4
eldrige <peldrige8@gmail.com>
2021-04-21 17:28:26 +0100

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import path from 'path';
+// import { dirname } from 'path';
 import cors from 'cors';
 import careerRouter from './routes/careerRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -8,18 +10,18 @@ import jobRouter from './routes/jobRoutes.js';
 import concourRouter from './routes/concourRoutes.js';
 import connectDB from './config/db.js';
 
+// import expose from './expose.js';
+const __dirname = path.resolve();
+
 dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// const __dirname = path.resolve(path.dirname(''));
 
 app.use(express.json());
 app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 
 app.use(cors());
 
@@ -27,6 +29,14 @@ app.use('/api/users/', userRouter);
 app.use('/api/careers/', careerRouter);
 app.use('/api/jobs/', jobRouter);
 app.use('/api/concours/', concourRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist/career-app/')));
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, 'frontend', 'dist', 'career-app', 'index.html')
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
