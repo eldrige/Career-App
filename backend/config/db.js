69197bc4c3f59c6de51db1fc9afc822cf5ad25a9
eldrige <peldrige8@gmail.com>
2021-04-22 09:59:15 +0100

    modified mongo connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1/Jongleur', {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://eldrige:baguvix75009@cluster0.juzpn.mongodb.net/Jongleur?retryWrites=true&w=majority',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`Mongo Db connected`);
   } catch (error) {
     console.error(error);
