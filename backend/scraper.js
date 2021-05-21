94fe2421f541bedc253e826032dfd33c5dd06030
eldrige <peldrige8@gmail.com>
2021-05-21 16:59:31 +0100

    initial fetch

--- backend/scraper.js
+++ backend/scraper.js
@@ -1,59 +1,28 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-const numberOfJobs = 20;
+const numberOfJobs = 1;
 const url = `https://www.cameroondesks.com/search/label/jobs?max-results=${numberOfJobs}`;
 
+var fetchedJobs = [];
 const fetchAndLoad = async (url) => {
   const { data: html } = await axios.get(url);
 
   if (html) {
     const $ = cheerio.load(html);
     const jobBlock = $('.post.hentry');
-    let jobs = [];
     jobBlock.each((idx, elt) => {
       let title = $(elt).find('h2').text().replace(/\s\s+/g, '');
       let link = $(elt).find('a').attr('href');
-      jobs.push({ link, title });
+      let datePublished = Date.now();
+      // let imageSrc = $(elt).find('.post-thumb').find('a').attr('style');
+      fetchedJobs.push({ link, title, datePublished });
       // let shortDescription = $(elt).find('article').text();
       // console.log(shortDescription);
     });
-    console.log(jobs);
-    // console.log(jobBlock.find('.post-summary').text());
+    // console.log(jobBlock.find('.post-summary').); the span containing the job description
     process.stdout.write('Scraping done...');
     // console.table(jobTitles);
   }
-
-  // request(url, (err, response, html) => {
-  //   if (!err && response.statusCode === 200) {
-  //     const $ = cheerio.load(html);
-  //     let titleArray = [];
-  //     $('.post.hentry').each((index, element) => {
-  //       const title = $(element).find('h2').text().replace(/\s\s+/g, '');
-
-  //       const link = $(element).find('a').attr('href');
-  //       // const description = $(element).find('div.resumo').text();
-  //       // console.log(title);
-
-  //       // console.log(title.split(" "))
-
-  //       let funnyArray = title.split(' ');
-
-  //       let singleJob = funnyArray.reduce((prev, next) => prev + next);
-  //       console.log(title);
-
-  //       // write to json
-  //       // writeStream.write(`${title}, ${link} \n`);
-  //     });
-  //     console.log('Scraping done .....');
-  //   } else {
-  //     throw error('An unexpected error occured');
-  //   }
-  // });
 };
 
 fetchAndLoad(url);
-
-// const createJobData = (titles, links) => {
-//   const jobs = [];
-
-// };
