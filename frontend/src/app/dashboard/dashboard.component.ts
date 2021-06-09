90a6eb5b5521878b44a29d31f717638a027ab45a
eldrige <peldrige8@gmail.com>
2021-06-09 23:38:28 +0100

    consume careers in dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
+import { CareerService } from './../shared/career.service';
 import { UserService } from './../services/user.service';
+
 import { Component, OnInit } from '@angular/core';
+import { ICareer } from '../shared/career';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,17 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private careerService: CareerService
+  ) {}
 
   users: any;
+  careers: ICareer[];
 
   ngOnInit(): void {
     this.getUsers();
+    this.getCareers();
   }
 
   getUsers() {
     return this.userService.getAllUsers().subscribe((response) => {
       this.users = response;
+    });
+  }
+
+  getCareers() {
+    return this.careerService.getCareers().subscribe((response) => {
+      this.careers = response;
     });
   }
 }
